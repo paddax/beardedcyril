19d49f21e609cdf13eeb32690dda5d53a8ed2c74
purple-alert <powder@andystokoe.me.uk>
2014-10-26 17:10:40 +0000

    added some JUnit test classes

---
+++ BOGGLE/test/com/powdermonkey/boggle/BoardTest.java
@@ -0,0 +1,34 @@
+package com.powdermonkey.boggle;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BoardTest {
+
+	@Test
+	public void testBoard() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testLoad() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetRoll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testShake() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
