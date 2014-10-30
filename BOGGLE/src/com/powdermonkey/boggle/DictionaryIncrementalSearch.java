e6c83ddeea1707c1e26fcc343ff9920aeccd141e
pdavis <paddax@gmail.com>
2014-10-30 07:45:45 +0000

    Added some documentation

--- BOGGLE/src/com/powdermonkey/boggle/DictionaryIncrementalSearch.java
+++ BOGGLE/src/com/powdermonkey/boggle/DictionaryIncrementalSearch.java
@@ -1,19 +1,37 @@
 package com.powdermonkey.boggle;
 
-import java.util.EnumSet;
-
 public class DictionaryIncrementalSearch {
 
+	/**
+	 * Internal index set by the IDictionary, used for optimisation
+	 */
 	public int index;
+
+	/**
+	 * After a call to lookup valid is true if the presented text is in the
+	 * dictionary
+	 */
 	public boolean valid;
+
+	/**
+	 * After a call the lookup partial is set to true if the presented word
+	 * could be valid if more characters were added.
+	 * 
+	 * Its also set to true at the same time as valid (bug / side effect /
+	 * feature)
+	 */
 	public boolean partial;
+
+	/**
+	 * The text to search
+	 */
 	public String text;
-	
+
 	public DictionaryIncrementalSearch() {
 		index = -1;
 		text = "";
 	}
-	
+
 	public DictionaryIncrementalSearch(String text, int index, boolean valid,
 			boolean partial) {
 		this.text = text;
--- BOGGLE/src/com/powdermonkey/boggle/IDictionary.java
+++ BOGGLE/src/com/powdermonkey/boggle/IDictionary.java
@@ -3,11 +3,41 @@ package com.powdermonkey.boggle;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * Common interface to the dictionary. This allows different dictionary
+ * implementation to be tried without too much effort
+ * 
+ * @author paddax@gmail.com
+ * 
+ */
 public interface IDictionary {
 
-	public abstract DictionaryIncrementalSearch lookup(DictionaryIncrementalSearch s);
+	/**
+	 * Attempts to find the word dis.text in the list
+	 * 
+	 * @param dis
+	 *            Search state thats updated by lookup for use in the next
+	 *            search
+	 * @return A reference to the dis parameter
+	 */
+	public abstract DictionaryIncrementalSearch lookup(
+			DictionaryIncrementalSearch dis);
 
+	/**
+	 * Loads the dictionary from a stream, each word is expected to be in
+	 * alphabetical order with a line seperator between each word.
+	 * 
+	 * <p>
+	 * <b>NOTE:</b> The reader ensures all the words
+	 * <ul>
+	 * <li>Are a single case.
+	 * <li>Greater than 2 characters long.
+	 * </ul>
+	 * 
+	 * @param is
+	 *            Stream containing words
+	 * @throws IOException
+	 */
 	public abstract void loadWords(InputStream is) throws IOException;
 
-	
 }
